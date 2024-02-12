e9c8addcd44d0c133424d72e91d05cfd51c23bc7
Terross <dima38091@gmail.com>
2024-02-12 23:47:02 +0300

    Start implementation main profile page

---
+++ apps/core/src/components/profile/ProfileInfoComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card>
+    <v-card-title>Карточка профиля</v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+      
+  },
+})
+</script>
