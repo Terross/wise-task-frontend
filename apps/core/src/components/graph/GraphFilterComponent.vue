31b98e84b9c02280c2babc5c6011d03e3c03d46f
Terross <dima38091@gmail.com>
2024-03-14 00:18:51 +0300

    End graph constructor

---
+++ apps/core/src/components/graph/GraphFilterComponent.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-card>
+		<v-card-title>Поиск графа</v-card-title>
+		<v-card-text>
+			<v-text-field label="Название графа">
+
+			</v-text-field>
+		</v-card-text>
+		<v-card-actions>
+			<v-btn>Найти</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+		
+	},
+})
+</script>
---
+++ apps/core/src/components/graph/GraphTableComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+		<v-data-table>
+			
+		</v-data-table>
+	</v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { storeToRefs } from 'pinia'
+
+
+export default defineComponent({
+	setup() {
+		const { graphLibrary } = storeToRefs(useGraphStore())
+
+		return {
+			graphLibrary
+		}
+	},
+})
+</script>
