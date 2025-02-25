98dd482948a87cf2dabd73541fd76d7a6a87ef9f
Egor Anakhin <eanahin8v@mail.ru>
2025-02-25 23:40:05 +0300

    feat: uploading info file

--- apps/core/src/features/graph/stores/nodes.ts
+++ apps/core/src/features/graph/stores/nodes.ts
@@ -1,47 +1,39 @@
 import { defineStore } from "pinia";
 import type { Node } from "@/features/graph/types/Node";
-import type { Edge, Connection } from "@vue-flow/core"
+import type { Edge } from "@vue-flow/core";
 
 interface NodesStoreState {
-    nodes: Node[]
-    edges: Edge[]
+  nodes: Node[];
+  edges: Edge[];
 }
 
 export const useNodeStore = defineStore("nodes", {
-    state: (): NodesStoreState => ({
-        nodes: [{
-            id: '1',
-            position: { x: 250, y: 5 },
-            data: { label: 'Node 1' },
-            type: "special"
-        }],
-        edges: []
-    }),
-    actions: {
-        addNode(): void {
-            const id: string = Date.now().toString()
-            this.nodes.push({
-                id: id,
-                position: { x: Math.random() * 400, y: Math.random() * 400 }, // Случайная позиция
-                type: "special",
-                data: { label: "Новая вершина" }
-            })
-        },
-        addEdge(edge: Edge): void {
-            // Проверка на существующее соединение
-            const exists = this.edges.some(
-                e => e.source === edge.source && e.target === edge.target
-            )
-
-            if (!exists) {
-                this.edges.push(edge)
-            }
-        },
-        renameNode(id: string, name: string): void {
-            const node = this.nodes.find(x => x.id === id)
-            if (node) {
-                node.data.label = name
-            }
-        }
-    }
-})
+  state: (): NodesStoreState => ({
+    nodes: [
+      {
+        id: "1",
+        position: { x: 250, y: 5 },
+        data: { label: "Node 1" },
+        type: "special",
+      },
+    ],
+    edges: [],
+  }),
+  actions: {
+    addNode(): void {
+      const id: string = Date.now().toString();
+      this.nodes.push({
+        id: id,
+        position: { x: Math.random() * 400, y: Math.random() * 400 }, // Случайная позиция
+        type: "special",
+        data: { label: "Новая вершина" },
+      });
+    },
+    renameNode(id: string, name: string): void {
+      const node = this.nodes.find((x) => x.id === id);
+      if (node) {
+        node.data.label = name;
+      }
+    },
+  },
+});
