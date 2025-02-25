83388b1e6a4b87c916eff7acc02bc2157e063770
Egor Anakhin <eanahin8v@mail.ru>
2025-02-25 15:08:27 +0300

    feat: new graph init

---
+++ src/pages/graph_test/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import {defineComponent} from "vue";
+  import Graph from "@/features/graph/ui/Graph.vue";
+
+  export default defineComponent({
+    components: {Graph}
+  })
+
+</script>
+
+
+<template>
+  <Graph/>
+</template>
