8695088e29620464476c33180deb4c35246588fe
Egor Anakhin <eanahin8v@mail.ru>
2025-03-12 21:09:56 +0300

    feat: added new graph to graph lib page

--- src/components/task/constructor/graph/GraphTaskConstructorComponent.vue
+++ src/components/task/constructor/graph/GraphTaskConstructorComponent.vue
@@ -1,52 +1,64 @@
 <template>
-    <v-container>
-        <v-row>
-            <v-col v-if="taskGraphConstructorInfo.isGraphPresent">
-                <control-panel-component></control-panel-component>
-				<graph-constructor-component></graph-constructor-component>
-            </v-col>
-            <v-col>
-                <v-textarea variant="outlined" auto-grow v-model="taskTextModel"></v-textarea>
-                <task-condition-component></task-condition-component>
-                <task-graph-plugin-table-component></task-graph-plugin-table-component>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row>
+      <v-col v-if="taskGraphConstructorInfo.isGraphPresent">
+        <Graph
+          :style="{
+            width: '100%',
+            height: '100%',
+            border: '1px solid black',
+            boxSizing: 'border-box',
+          }"
+        />
+      </v-col>
+      <v-col>
+        <v-textarea
+          variant="outlined"
+          auto-grow
+          v-model="taskTextModel"
+        ></v-textarea>
+        <task-condition-component></task-condition-component>
+        <task-graph-plugin-table-component></task-graph-plugin-table-component>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
-import { usePluginStore } from '@/store/plugin';
-import { storeToRefs } from 'pinia';
-import { useQuery } from '@vue/apollo-composable';
-import { GET_ALL_PLUGINS } from '@/api/Queries';
-import { useTaskStore } from '@/store/task';
-
+import { computed, defineComponent } from "vue";
+import { usePluginStore } from "@/store/plugin";
+import { storeToRefs } from "pinia";
+import { useQuery } from "@vue/apollo-composable";
+import { GET_ALL_PLUGINS } from "@/api/Queries";
+import { useTaskStore } from "@/store/task";
+import Graph from "@/features/graph/ui/Graph.vue";
 
 export default defineComponent({
-    setup() {
-        const { plugins } = storeToRefs(usePluginStore())
-		const { onResult } = useQuery(GET_ALL_PLUGINS)
-        const { taskGraphInput, taskGraphConstructorInfo } = storeToRefs(useTaskStore())
-		onResult(response => {
-			if (response.data) {
-				plugins.value = response.data.getAllPlugins
-			}
-		})
+  components: { Graph },
+  setup() {
+    const { plugins } = storeToRefs(usePluginStore());
+    const { onResult } = useQuery(GET_ALL_PLUGINS);
+    const { taskGraphInput, taskGraphConstructorInfo } =
+      storeToRefs(useTaskStore());
+    onResult((response) => {
+      if (response.data) {
+        plugins.value = response.data.getAllPlugins;
+      }
+    });
 
-        const taskTextModel = computed({
-            get() {
-                return taskGraphInput.value.description
-            },
-            set(value) {
-                taskGraphInput.value.description = value
-            }
-        })
+    const taskTextModel = computed({
+      get() {
+        return taskGraphInput.value.description;
+      },
+      set(value) {
+        taskGraphInput.value.description = value;
+      },
+    });
 
-        return {
-            taskTextModel,
-            taskGraphConstructorInfo
-        }
-    },
-})
+    return {
+      taskTextModel,
+      taskGraphConstructorInfo,
+    };
+  },
+});
 </script>
--- src/features/graph/ui/Graph.vue
+++ src/features/graph/ui/Graph.vue
@@ -5,6 +5,12 @@ import SpecialNode from "./SpecialNode.vue";
 import SpecialEdge from "./SpecialEdge.vue";
 import { useNodeStore } from "@/features/graph/stores/nodes";
 
+interface Props {
+  style?: Record<string, string | number>;
+}
+
+const props = defineProps<Props>();
+
 const nodeStore = useNodeStore();
 
 const { onConnect, addEdges, onNodeDragStop, onConnectEnd } = useVueFlow();
@@ -46,26 +52,28 @@ const downloadJson = () => {
 </script>
 
 <template>
-  <div class="buttons-container">
-    <v-btn @click="nodeStore.addNode">Добавить вершину</v-btn>
-    <v-btn @click="nodeStore.undo">UNDO</v-btn>
-    <v-btn @click="printNodesAndEdges">Вывести ноды и ребра</v-btn>
-    <v-btn @click="downloadJson">Скачать JSON</v-btn>
-    <v-btn @click="nodeStore.toggleIsDirected">Сменить мод</v-btn>
+  <div :style="props.style">
+    <div class="buttons-container">
+      <v-btn @click="nodeStore.addNode">Добавить вершину</v-btn>
+      <v-btn @click="nodeStore.undo">UNDO</v-btn>
+      <v-btn @click="printNodesAndEdges">Вывести ноды и ребра</v-btn>
+      <v-btn @click="downloadJson">Скачать JSON</v-btn>
+      <v-btn @click="nodeStore.toggleIsDirected">Сменить мод</v-btn>
+    </div>
+    <VueFlow
+      :connection-radius="30"
+      v-model:nodes="nodeStore.nodes"
+      v-model:edges="nodeStore.edges"
+      class="pinia-flow"
+    >
+      <template #node-special="specialNodeProps">
+        <SpecialNode v-bind="specialNodeProps" />
+      </template>
+      <template #edge-special="specialEdgeProps">
+        <SpecialEdge v-bind="specialEdgeProps" />
+      </template>
+    </VueFlow>
   </div>
-  <VueFlow
-    :connection-radius="30"
-    v-model:nodes="nodeStore.nodes"
-    v-model:edges="nodeStore.edges"
-    class="pinia-flow"
-  >
-    <template #node-special="specialNodeProps">
-      <SpecialNode v-bind="specialNodeProps" />
-    </template>
-    <template #edge-special="specialEdgeProps">
-      <SpecialEdge v-bind="specialEdgeProps" />
-    </template>
-  </VueFlow>
 </template>
 
 <style>
@@ -73,7 +81,16 @@ const downloadJson = () => {
 @import "@vue-flow/core/dist/theme-default.css";
 
 .buttons-container {
+  background-color: blue;
+  padding: 5px 10px;
   display: flex;
   column-gap: 10px;
+}
+
+.pinia-flow {
+  position: relative;
+  width: 100%;
+  height: calc(100% - 45px); // TODO: сделать нормально высоту
+  overflow: hidden;
 }
 </style>
