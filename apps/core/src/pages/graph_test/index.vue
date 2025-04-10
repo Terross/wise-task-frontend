b1cd082a4efb721f5b19207b3525322396b3dde3
Egor Anakhin <eanahin8v@mail.ru>
2025-04-10 21:05:02 +0300

    feat: is nearly full graph

--- apps/core/src/pages/graph_test/index.vue
+++ apps/core/src/pages/graph_test/index.vue
@@ -1,14 +1,18 @@
 <script lang="ts">
-  import {defineComponent} from "vue";
-  import Graph from "@/features/graph/ui/Graph.vue";
+import { defineComponent } from "vue";
+import Graph from "@/features/graph/ui/Graph.vue";
 
-  export default defineComponent({
-    components: {Graph}
-  })
-
+export default defineComponent({
+  components: { Graph },
+});
 </script>
 
-
 <template>
-  <Graph/>
+  <Graph
+    :style="{
+      width: '100%',
+      height: '100%',
+      boxSizing: 'border-box',
+    }"
+  />
 </template>
