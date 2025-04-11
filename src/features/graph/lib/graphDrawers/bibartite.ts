ecb7390ed1a67bd9a21dd6aeb69315138b7853b4
Egor Anakhin <eanahin8v@mail.ru>
2025-04-11 22:09:54 +0300

    feat: base bipartite graph normalizing

--- src/features/graph/lib/graphDrawers/bibartite.ts
+++ src/features/graph/lib/graphDrawers/bibartite.ts
@@ -4,11 +4,14 @@ import {
   ConnectionTargetID,
   CustomEdge,
 } from "@/features/graph/types/CustomEdge";
-import { buildLinkedListFromGraph } from "@/features/graph/lib/helpers/linkedListFromGraph";
-import { DRAW_SPACING_X } from "@/features/graph/config/drawSpacing";
+import { DRAW_SPACING_Y } from "@/features/graph/config/drawSpacing";
 import { DEFAULT_NODE_SIZE } from "@/features/graph/config/nodeDefaultSettings";
 import { DrawerResults } from "@/features/graph/types/ConnectedComponents";
 import { colorNodesIntoTwoColors } from "@/features/graph/lib/helpers/colorNodesTwoColors";
+import {
+  getMinHeight,
+  getVerticalSpacing,
+} from "@/features/graph/lib/helpers/getBipartiteGraphHeight";
 
 export const drawBipartiteGraph = (
   nodes: CustomNode[],
@@ -16,20 +19,54 @@ export const drawBipartiteGraph = (
 ): DrawerResults => {
   let currentX: number = 0;
   let currentY: number = 0;
-  let xPositionTaken = 0;
-  let yPositionTaken = 0;
   const { redNodes, blueNodes } = colorNodesIntoTwoColors(edges, nodes);
+  const minHeight = Math.max(
+    getMinHeight(redNodes, DRAW_SPACING_Y),
+    getMinHeight(blueNodes, DRAW_SPACING_Y),
+  );
+  const redsVerticalSpacing = getVerticalSpacing(redNodes, minHeight);
+  const bluesVerticalSpacing = getVerticalSpacing(blueNodes, minHeight);
 
-  redNodes.forEach((node) => {
-    console.log("red", node.data.label);
+  const redNodeIds = new Set(redNodes.map((node) => node.id));
+  const blueNodeIds = new Set(blueNodes.map((node) => node.id));
+
+  edges.forEach((edge) => {
+    const isRedToBlue =
+      redNodeIds.has(edge.source) && blueNodeIds.has(edge.target);
+    const isBlueToRed =
+      blueNodeIds.has(edge.source) && redNodeIds.has(edge.target);
+
+    if (isRedToBlue) {
+      edge.sourceHandle = ConnectionSourceID.Right;
+      edge.targetHandle = ConnectionTargetID.Left;
+    } else {
+      edge.sourceHandle = ConnectionSourceID.Left;
+      edge.targetHandle = ConnectionTargetID.Right;
+    }
   });
 
-  blueNodes.forEach((node) => {
-    console.log("blue", node.data.label);
-  });
+  const redNodesNormalized: CustomNode[] = [];
+  for (let i = 0; i < redNodes.length; i++) {
+    redNodes[i].position.y = currentY;
+    redNodes[i].position.x = currentX;
+    redNodesNormalized.push(redNodes[i]);
+    currentY += redNodes[i].data.size?.height || DEFAULT_NODE_SIZE.height;
+    currentY += redsVerticalSpacing;
+  }
+
+  currentY = 0;
+
+  const blueNodesNormalized: CustomNode[] = [];
+  for (let i = 0; i < blueNodes.length; i++) {
+    blueNodes[i].position.y = currentY;
+    blueNodes[i].position.x = currentX + 300;
+    blueNodesNormalized.push(blueNodes[i]);
+    currentY += blueNodes[i].data.size?.height || DEFAULT_NODE_SIZE.height;
+    currentY += bluesVerticalSpacing;
+  }
 
   return {
-    nodes,
+    nodes: [...redNodesNormalized, ...blueNodesNormalized],
     edges,
     xFinish: currentX,
     yFinish: currentY,
