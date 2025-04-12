36b6df857700ea5a1250da1c2d72231fe90023b1
Egor Anakhin <eanahin8v@mail.ru>
2025-04-12 23:53:04 +0300

    feat: default graph normalizing type

---
+++ apps/core/src/features/graph/lib/graphDrawers/default.ts
@@ -0,0 +1,31 @@
+import { CustomNode } from "@/features/graph/types/CustomNode";
+import { CustomEdge } from "@/features/graph/types/CustomEdge";
+import { DrawerResults } from "@/features/graph/types/ConnectedComponents";
+import { DEFAULT_NODE_SIZE } from "@/features/graph/config/nodeDefaultSettings";
+import { DRAW_SPACING_X } from "@/features/graph/config/drawSpacing";
+
+export const drawDefaultTypeGraph = (
+  nodes: CustomNode[],
+  edges: CustomEdge[],
+): DrawerResults => {
+  let width = 0;
+  let height = 0;
+
+  nodes.forEach((node) => {
+    width +=
+      (node.data.size?.width || DEFAULT_NODE_SIZE.width) + DRAW_SPACING_X;
+    height = Math.max(
+      height,
+      node.data.size?.height || DEFAULT_NODE_SIZE.height,
+    );
+  });
+
+  width -= DRAW_SPACING_X;
+
+  return {
+    nodes,
+    edges,
+    width,
+    height,
+  };
+};
