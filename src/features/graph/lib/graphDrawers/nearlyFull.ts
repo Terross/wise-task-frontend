266e6545e96e8125ded17028d719acc9c5f86ad0
Egor Anakhin <eanahin8v@mail.ru>
2025-04-14 00:50:12 +0300

    feat: circular graph edges position change

--- src/features/graph/lib/graphDrawers/nearlyFull.ts
+++ src/features/graph/lib/graphDrawers/nearlyFull.ts
@@ -1,12 +1,9 @@
 import { CustomNode } from "@/features/graph/types/CustomNode";
 import { CustomEdge } from "@/features/graph/types/CustomEdge";
-import {
-  DEFAULT_CIRCLE_PADDING,
-  DRAW_SPACING_X,
-} from "@/features/graph/config/drawParams";
+import { DEFAULT_CIRCLE_PADDING } from "@/features/graph/config/drawParams";
 import { DrawerResults } from "@/features/graph/types/ConnectedComponents";
 import { generateCircleParams } from "@/features/graph/lib/helpers/calculateCirleParams";
-import { DEFAULT_NODE_SIZE } from "@/features/graph/config/nodeDefaultSettings";
+import { changeDirectionsInCircularGraph } from "@/features/graph/lib/helpers/edgesDirectionsInCircular";
 
 export const drawNearlyFullGraph = (
   nodes: CustomNode[],
@@ -28,14 +25,11 @@ export const drawNearlyFullGraph = (
 
     node.position.x = centerX + Math.cos(angle) * radius;
     node.position.y = centerY + Math.sin(angle) * radius;
-
-    const nodeHeight: number =
-      node.data.size?.height || DEFAULT_NODE_SIZE.height;
   });
 
   return {
     nodes,
-    edges,
+    edges: changeDirectionsInCircularGraph(nodes, edges),
     width: radius * 2,
     height: radius * 2,
   };
