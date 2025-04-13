af19157103743a6abca89b57d68ab91decd1933b
Egor Anakhin <eanahin8v@mail.ru>
2025-04-13 17:54:13 +0300

    feat: history limit

--- src/features/graph/lib/history/history.ts
+++ src/features/graph/lib/history/history.ts
@@ -1,24 +1,28 @@
-import {RestoreObject} from "@/features/graph/types/RestoreObject";
-import {Restore} from "@/features/graph/types/restores";
-import {RestoreObjectFactory} from "@/features/graph/lib/history/factory";
-import {NodesStoreState} from "@/features/graph/types/NodesStore";
+import { RestoreObject } from "@/features/graph/types/RestoreObject";
+import { Restore } from "@/features/graph/types/restores";
+import { RestoreObjectFactory } from "@/features/graph/lib/history/factory";
+import { NodesStoreState } from "@/features/graph/types/NodesStore";
 
 class History {
-    private restores: RestoreObject[] = []
-    public constructor() {
-    }
+  private restores: RestoreObject[] = [];
+  private readonly MAX_HISTORY_LENGTH: number = 100;
 
-    public onStateUpdate = (restore: Restore) => {
-        this.restores.push(RestoreObjectFactory.create(restore))
-    }
+  public constructor() {}
 
-    public undo = (state: NodesStoreState): NodesStoreState => {
-        const restoreObject = this.restores.pop()
-        if (!restoreObject) {
-            return state
-        }
-        return restoreObject.restore(state)
+  public onStateUpdate = (restore: Restore) => {
+    this.restores.push(RestoreObjectFactory.create(restore));
+    if (this.restores.length > this.MAX_HISTORY_LENGTH) {
+      this.restores.shift();
     }
+  };
+
+  public undo = (state: NodesStoreState): NodesStoreState => {
+    const restoreObject: RestoreObject | undefined = this.restores.pop();
+    if (!restoreObject) {
+      return state;
+    }
+    return restoreObject.restore(state);
+  };
 }
 
-export const history: History = new History()
+export const history: History = new History();
